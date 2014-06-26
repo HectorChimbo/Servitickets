637d580427a9158a551a2951603d13ded695ff10
geankrlo <geankrlo22@gmail.com>
2014-06-26 00:34:47 -0500

    quienesSomos.php
    página de quienes somos

---
+++ quienesSomos.php
@@ -0,0 +1,23 @@
+<html>
+<? php include ('metadatos.php');?>
+<body>
+	<div class="container_24">
+		<?php include('header.php');?>
+		<div class="clear"></div>
+		<?php include('menu.php');?>
+		<div class="clear"></div>
+		<?php include('banner.php');?>
+		<div class="clear"></div>
+		<div id="quienesSomos" class="grid_16">
+		<h1>Quienes Somos</h1><br/>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mi et dui pellentesque tincidunt eget sit amet dui. In hac habitasse platea dictumst. Vivamus in congue nisl, non faucibus sapien. Fusce mi lectus, molestie eu volutpat adipiscing, feugiat vel lorem. Curabitur at quam luctus libero consectetur molestie.</p><br/>
+		<h1>Misi&oacute;n</h1><br/>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mi et dui pellentesque tincidunt eget sit amet dui. In hac habitasse platea dictumst. Vivamus in congue nisl, non faucibus sapien. Fusce mi lectus, molestie eu volutpat adipiscing, feugiat vel lorem. Curabitur at quam luctus libero consectetur molestie.</p><br/>
+		<h1>Visi&oacute;n</h1><br/>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mi et dui pellentesque tincidunt eget sit amet dui. In hac habitasse platea dictumst. Vivamus in congue nisl, non faucibus sapien. Fusce mi lectus, molestie eu volutpat adipiscing, feugiat vel lorem. Curabitur at quam luctus libero consectetur molestie.</p><br/>
+		</div>
+		<div class="clear"></div>
+		<?php include('footer.php');?>
+	</div>
+</body>
+</html>
