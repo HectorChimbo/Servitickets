59a3295651354270f3a9fd624edaeff72a311861
RebeKris Ramirez <cris.rebek92@gmail.com>
2014-07-20 23:31:08 -0500

    Update index.php
    Nueva disposicion del home, plantilla con disenho adaptable

--- index.php
+++ index.php
@@ -1,25 +1,79 @@
+<!doctype html>
 <html>
-<? php include ('metadatos.php');?>
-<body>
-<div class="container_24">
-	<?php include('header.php');?>
-	<div class="clear"></div>
-	<?php include('menu.php');?>
-	<div class="clear"></div>
-	<?php include('banner.php');?>
-	<div class="clear"></div>
-	<div id="contenedor_izquierdo" class="grid_16">
-	<div class="clear"> </div>
-	<div id="contenedor_izquierdo" class="grid_16"> </div>  
-	<?php include('main.php');?>
-	</div>
-<div class="clear"></div>
-	<?php include('footer.php');?>
-	
-<!-- <div id="contenedor_derecho" class="grid_8">
-		<iframe width="100%" height="250" src="//www.youtube.com/embed/mBwsUD7jYCI?autoplay=0&color=white&showinfo=0&controls=0" frameborder="0" allowfullscreen></iframe></div>	-->
+	<?php include ('metadatos.php');?>
+	<body class="fondo">
+
+		<!-- HEADER SECTION -->
+		<div class="navbar navbar-fixed-top navbar-inverse" role="">
+
+			<!-- TOPBAR SECTION -->
+			<nav class="top-bar important-class" data-topbar>
+
+				<!-- TITLE AREA & LOGO -->
+				<ul class="title-area">
+					<li class="name">
+						<img src="img/cabecera.png" alt="" id="logo-image">
+
+					</li>
+					<li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
+				</ul> <!-- END TITLE AREA & LOGO -->
+
+				<!-- MENU ITEMS -->
+				<section class="top-bar-section">
+					<ul class="right">
+						<li class="active"><a href="index.php">INICIO</a></li>
+						<!--li><a href="#">Portfolio</a></li-->
+						<li class="active"><!-- class="has-dropdown"-->
+							<a href="noticias.php">NOTICIAS</a>
+							<!--ul class="dropdown">
+								<li><a href="#">NOTICIA #1</a></li>
+								<li><a href="#">NOTICIA #2</a></li>
+								<li><a href="#">NOTICIA #N</a></li>
+							</ul-->
+						</li>
+                        <li class="active"><a href="#">PRODUCTOS</a></li>
+						<li class="active"><a href="#">CONT&Aacute;CTENOS</a></li>
+					</ul>
+				</section> <!-- END MENU ITEMS -->
+			</nav> <!-- END TOPBAR SECTION -->
+		</div> <!-- END HEADER SECTION
+
+		<!-- CONTENT FILL WHEN SCROLL = 0 -->
 		
+ 
+
+		<!-- CONTENT SECTION -->
+		<div class="row content-section">
+        
+        
+           
+			<div class="main-content container">
+            <div class="container"> 
+			<?php include('banner.php');?>
+            </div>
+            	
+			  <div class=" small-12 medium-12 large-12 columns">
+                	
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, fugiat, explicabo architecto at praesentium modi soluta alias veniam voluptate magnam obcaecati facilis cumque voluptatum impedit eius reiciendis ad minus odio similique sequi molestiae voluptatem commodi aliquam assumenda numquam doloribus libero tempora vitae sed animi. Ratione, quasi iste fugiat debitis molestiae doloremque consequuntur hic delectus molestias repudiandae reprehenderit rerum reiciendis enim aperiam cum ut adipisci veritatis soluta illo praesentium consequatur voluptate possimus dolorem et officiis perspiciatis commodi quae eligendi alias culpa atque ducimus eum odit accusamus ad labore quod necessitatibus. Officiis, ducimus, voluptate doloremque nihil rerum consequuntur obcaecati fugiat cum veritatis quidem qui nulla distinctio laborum perspiciatis quia tempora eveniet tempore aliquam pariatur aliquid quibusdam aspernatur quis maiores minus quas culpa reprehenderit id iure optio asperiores? Excepturi, deleniti, at, expedita quod animi harum est sit iste distinctio consequuntur mollitia corporis soluta. Quos, asperiores voluptas totam nesciunt atque iusto commodi. Autem, animi eius odio commodi vitae nihil possimus neque minima obcaecati aspernatur voluptate dolorem consequuntur illo vel iste deserunt tempore eaque maxime cum ratione rem odit officia tempora ut recusandae saepe maiores quae repellat debitis atque est praesentium numquam mollitia dolor ipsum modi fuga veniam error perspiciatis quas provident optio similique iure.</p>
+
+				
+				</div>
+			</div>
+		</div> <!-- END CONTENT SECTION -->
+
+
+		<!-- FOOTER SECTION -->
+		<div class="footer-section">
+			<div class="row">
+				<div class="small-12 medium-12 large-12 columns">
+					<p>EScoffee &copy; Copyright 2014</p>
+				</div>
+			</div>
+		</div> <!-- END FOOTER SECTION -->
+
+		<!-- JAVASCRIPTS -->
 		
-</div>
-</body>
+	</body>
 </html>
+
