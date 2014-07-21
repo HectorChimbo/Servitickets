9ee122d12662587b77a611621ef1eebbda5b1c04
RebeKris Ramirez <cris.rebek92@gmail.com>
2014-07-20 23:47:05 -0500

    Update metadatos.php

--- metadatos.php
+++ metadatos.php
@@ -1,19 +1,52 @@
 <?php
 echo <<< HERE
-	<head>
-	  <title>SERVITICKETS</title>
-  	  <link rel="stylesheet" type="text/css" href="css/960_24_col.css" />
-	  <link rel="stylesheet" type="text/css" href="css/estilos.css" />
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<meta rel="shortcut icon" href="favicon.ico" > 
+	<title>Servitickets</title>
 	
-	  <script src="js/modernizr-2.6.1.min.js"></script>
-    	  <script src="js/jquery.js"></script>
-    	  <script src="slideshow/lean-slider.js"></script>
-    	  <link rel="stylesheet" href="slideshow/lean-slider.css" type="text/css" />   
-	  <link rel="stylesheet" href="slideshow/sample-styles.css" type="text/css" />
-	  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
-	</head>
+	
+    <link rel="stylesheet" href="css/app.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="css/960_24_col.css"/>
+    <link rel="stylesheet" href="lib/slideshow/lean-slider.css"/>   
+    <link rel="stylesheet" href="lib/slideshow/sample-styles.css" />
+    <link rel="stylesheet" href="lib/Lightbox/nivo-lightbox.css"/> 
+	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css"  type="text/css">
+	<link rel="stylesheet" href="css/application-5726a1497b01765265157d44c346f255.css" media="screen"/>
+
+
+	<script src="bower_components/modernizr/modernizr.js"></script>
+	<script src="js/jquery.js"></script>
+    <script src="lib/slideshow/lean-slider.js"></script>
+    <script src="lib/Lightbox/nivo-lightbox.js"></script>
+    <script src="lib/Lightbox/nivo-lightbox.min.js"></script>
+    <script src="https://dhchbx0tszrcx.cloudfront.net/assets/head.load-743386f0cd70fa6fad141cf9df072ab3.js"> </script>
+    <script src="js/bugsnag-75a3cec336c71fb66f1f02d6c57d69fb.js"> </script>
+    
+    
+
+
+	<script src="bower_components/foundation/js/foundation.min.js"></script>
+	<script src="/js/app.js"></script>
+	<!-- OUR JQUERY MAGIC -->
+	<script src="js/init.js"></script>
+       <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+    <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+
+    
+
+</head>
 
 HERE;
 
 ?>
+
+
+
 
