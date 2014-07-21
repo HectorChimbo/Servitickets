749ff5f9d1032ec6c4d0ac935c2c6abb0049b85a
RebeKris Ramirez <cris.rebek92@gmail.com>
2014-07-20 23:43:24 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,32 +1,28 @@
+
 <?php
-echo <<<HERE
-	<foot id="pie_pagina" class="grid_24">
-		<div id="menu_secundario" class="grid_7 alpha">
-			Men&uacute;
-            </br>
-            <ul>	
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Servitickets</a></li>
-                <li><a href="#">Compra en L&iacute;nea</a></li>
-                <li><a href="#">Cronograma de Evento</a></li>
-                <li><a href="#">Puntos de Venta</a></li>
-		</ul>	
-		</div>
-		<div id="ubiquenos" class="grid_8">
-			Ub&iacute;quenos
-             </br>
-            <iframe
-                 width="100%"
-                 height="150px"
-                 frameborder="0" style="border:0"
-                 src="https://mapsengine.google.com/map/embed?mid=zpT1ClwG7VTs.key3kKc2p-ac">
-                </iframe>
-		</div>
-		<div id="redes_sociales" class="grid_9 omega">
-			Redes Sociales
-             </br>
-            <p> <img id="sociales" src="img/1403177005_social_3.png"/>  <img id="sociales" src="img/1403177006_social_8.png"/>  <img id="sociales" src="img/1403177013_google.png"/> </br> <img id="sociales" src="img/1403177009_social_4.png"/>  <img id="sociales" src="img/1403177016_social_2.png"/> <img id="sociales" src="img/1403177467_647403-email.png"/></p>
-		</div>
-	</foot>	
+echo <<< HERE
+
+			<div class="navbar navbar-fixed-bottom row">
+				<div class="small-12 medium-12 large-12 columns">
+				<div class="navbar-left">
+					<p>Servitickets &copy; Copyright 2014</p>
+				</div>
+					
+					<div class="navbar-right ">
+						<ul class="nav navbar-nav">
+							<li> <a class="footer-nav__link footer-nav__link--icon footer-nav__link--icon-facebook" href="http://facebook.com/escoffee" target="_blank" title="Facebook Page"><img class="img-sensible img-responsible" src="img/fb.png"> </img></a> </li>
+							<li> <a class="footer-nav__link footer-nav__link--icon footer-nav__link--icon-twitter" href="http://twitter.com/escoffee" target="_blank" title="Facebook Page"><img class="img-sensible img-responsible" src="img/tw.png"> </img></a> </li>
+							<li> <a class="footer-nav__link footer-nav__link--icon footer-nav__link--icon-facebook" href="http://facebook.com/fliplingo" target="_blank" title="Facebook Page"><img class="img-sensible img-responsible" src="img/yt.png"> </img></a> </li>
+							<li> <a class="footer-nav__link footer-nav__link--icon footer-nav__link--icon-facebook" href="http://facebook.com/fliplingo" target="_blank" title="Facebook Page"><img class="img-sensible img-responsible" src="img/pintr.png"> </img></a> </li>
+							<li> <a class="footer-nav__link footer-nav__link--icon footer-nav__link--icon-facebook" href="http://facebook.com/fliplingo" target="_blank" title="Facebook Page"><img class="img-sensible img-responsible" src="img/inst.png"> </img></a> </li>
+						</ul>
+
+				
+					</div>
+				</div>
+			</div>
+
+
+
 HERE;
 ?>
